a8fc22235e183c982a62d0f071711906e9879298
Kevin Wang <kvnwang98@gmail.com>
2023-10-01 14:55:21 -0700

    sync

--- run_tdvp_gpu_nersc.sh
+++ run_tdvp_gpu_nersc.sh
@@ -9,7 +9,7 @@
 
 export SLURM_CPU_BIND="cores"
 
-julia $1 $2 $3 $4 $5 > logs_gpu/C${2}_J${3}_chi${4}_dt${5}.txt
+julia $1 $2 $3 $4 $5 $6 $7 $8 > logs_gpu/C${2}_L${3}_J${4}_B${5}_1Delta${6}_2Delta${6}_chi${7}_dt${8}.txt
 # julia $1 $2 $3 $4 $5 > logs_gpu/square_C${2}_chi${4}_dt${5}_double_evolve.txt
 
 exit 0
