e15f06ca4c1ebccf0dba625819d7cf1998214fd6
Kevin Wang <kvnwang98@gmail.com>
2025-04-15 18:15:17 -0700

    sync

--- ground_state_search.jl
+++ ground_state_search.jl
@@ -115,7 +115,7 @@ end
 function main(; C=4, L=6, J1=1.0, J2=0.0, B=0.0, Δ1=1.0, Δ2=1.0, cutoff=1f-6, maxdim=32)
     N = C * L
 
-    filename = "/pscratch/sd/k/kwang98/QSL/production/ground_state_search_C$(C)_L$(L)_J$(J2)_B$(B)_1Delta$(Δ1)_2Delta$(Δ2)_chi$(maxdim).h5"
+    filename = "/pscratch/sd/k/kwang98/QSL/ground_state_search_C$(C)_L$(L)_J$(J2)_B$(B)_1Delta$(Δ1)_2Delta$(Δ2)_chi$(maxdim).h5"
     sites = siteinds("S=1/2", N; conserve_qns=false)
     H = cu(MPO(triangular_model(C, L, J1, J2, B, Δ1, Δ2), sites))
 
